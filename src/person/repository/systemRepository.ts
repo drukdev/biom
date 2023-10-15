592379fc384589f0b78ad3285b3cd02a756a02d1
Yogesh Pawar <107561764+yogeshayanworks@users.noreply.github.com>
2023-10-15 12:12:22 +0530

    feat: NDI citizen API integration for to get bhutanese person image (#29)

--- src/person/repository/systemRepository.ts
+++ src/person/repository/systemRepository.ts
@@ -26,7 +26,6 @@ export class SystemRepository {
 
   async getCitizenImg(biometricReq: BiometricReq): Promise<string | undefined> {
     const ndiLogger = this.ndiLogger.getLoggerInstance(this.als);
-    ndiLogger.log(`start to get Citizen Image`);
     try {
       if (null != biometricReq.idNumber) {
         // Get access token
@@ -75,39 +74,54 @@ export class SystemRepository {
   async callSystemToGetPerson(token: string, idNumber: string, idType: IdTypes): Promise<PersonDTO | Person> {
     const ndiLogger = this.ndiLogger.getLoggerInstance(this.als);
     ndiLogger.log(`started calling system for idType : ${idType}`);
-    let systemurl: string = this.configService.get('CITIZEN_IMG') || '';
-    // Get Image by Work Permit
-    if (idType.toLowerCase().match(IdTypes.WorkPermit.toLowerCase())) {
-      systemurl = this.configService.get('IMMI_IMG') || '';
+    let systemUrl: string;
+
+    switch (idType.toLowerCase()) {
+      // Get Image by Citizenship
+      case IdTypes.Citizenship.toLowerCase():
+        systemUrl = `${this.configService.get('NDI_CITIZEN_IMG')}${idNumber}`;
+        ndiLogger.log(`Started to get citizen image using NDI_CITIZEN_IMG API`);
+        break;
+      // Get Image by Work Permit
+      case IdTypes.WorkPermit.toLowerCase():
+        systemUrl = `${this.configService.get('IMMI_IMG')}${idNumber}`;
+        ndiLogger.log(`Started to get immigrant image using IMMI_IMG API`);
+        break;
+      // Get Image by Passport number
+      case IdTypes.Passport.toLowerCase():
+        systemUrl = `${this.configService.get('IMM_IMG_PP')}${idNumber}`;
+        ndiLogger.log(`Started to get immigrant image using IMM_IMG_PP API`);
+        break;
+      default:
+        throw new HttpException('IdType is invalid', HttpStatus.BAD_REQUEST);
     }
-    // Get Image by Passport number
-    if (idType.toLowerCase().match(IdTypes.Passport.toLowerCase())) {
-      systemurl = this.configService.get('IMM_IMG_PP') || '';
-    }
-    systemurl = `${systemurl}${idNumber}`;
-    ndiLogger.log(`started calling system : url : ${systemurl}`);
+
     try {
-      const response: Person | PersonDTO = await this.getUserImage(token, systemurl, idType);
+      const response: Person | PersonDTO = await this.getUserImage(token, systemUrl, idType);
 
       return response;
     } catch (error) {
-
-      if (IdTypes.Citizenship === idType && error.toString().includes(HttpStatus.INTERNAL_SERVER_ERROR)) {
+      if (IdTypes.Citizenship.toLocaleLowerCase() === idType.toLocaleLowerCase()) {
         try {
-          ndiLogger.log('Getting royal user data');
-          systemurl = `${this.configService.get('ROYAL_IMG')}${idNumber}`;
-          const response: Person | PersonDTO = await this.getUserImage(token, systemurl, idType);
-          return response;
-        } catch (err) {
-        ndiLogger.error(`ERROR in getting royal user image : ${JSON.stringify(error)}`);
-          this.checkError(err);
+          ndiLogger.log(`Started to get citizen image using CITIZEN_IMG API`);
+          systemUrl = `${this.configService.get('CITIZEN_IMG')}${idNumber}`;
+          return this.getUserImage(token, systemUrl, idType);
+        } catch (error) {
+          try {
+            ndiLogger.log('Getting royal user data');
+            systemUrl = `${this.configService.get('ROYAL_IMG')}${idNumber}`;
+            const response: Person | PersonDTO = await this.getUserImage(token, systemUrl, idType);
+            return response;
+          } catch (err) {
+            ndiLogger.error(`ERROR in getting royal user image : ${JSON.stringify(error)}`);
+            this.checkError(err);
+          }
         }
       }
       ndiLogger.error(`ERROR in rest request : ${JSON.stringify(error)}`);
 
       this.checkError(error);
     }
-    
   }
   checkError(error): void {
     if (error.toString().includes(CommonConstants.RESP_ERR_HTTP_INVALID_HEADER_VALUE)) {
@@ -156,10 +170,10 @@ export class SystemRepository {
     }
   }
 
-  async getUserImage(token: string, systemurl: string, idType: IdTypes): Promise<Person | PersonDTO> {
+  async getUserImage(token: string, systemUrl: string, idType: IdTypes): Promise<Person | PersonDTO> {
     return lastValueFrom(
       this.httpService
-        .get(systemurl, { headers: { Authorization: `Bearer ${token}` } })
+        .get(systemUrl, { headers: { Authorization: `Bearer ${token}` } })
         .pipe(map((response) => response.data))
     ).then((data: Person) => this.getData(data, idType));
   }
@@ -167,7 +181,16 @@ export class SystemRepository {
   getData(data: any, system: string): Person | PersonDTO {
     let result: Person | PersonDTO;
     if (system.match(IdTypes.Citizenship)) {
-      result = 0 < Object.keys(data['citizenimages']).length ? data['citizenimages']['citizenimage'][0] : undefined;
+      if (data['ndicitizenimages']) {
+        if (0 < Object.keys(data['ndicitizenimages']).length && data['ndicitizenimages']['ndicitizenimage'] && data['ndicitizenimages']['ndicitizenimage'][0]) {
+          // eslint-disable-next-line prefer-destructuring
+          result = data['ndicitizenimages']['ndicitizenimage'][0];
+        } else {
+          throw new HttpException('NDI citizen image not found', HttpStatus.BAD_REQUEST);
+        }
+      } else if (data['citizenimages']) {
+        result = 0 < Object.keys(data['citizenimages']).length ? data['citizenimages']['citizenimage'][0] : undefined;
+      }
     }
     if (system.match(IdTypes.WorkPermit)) {
       result = 0 < Object.keys(data['ImmiImages']).length ? data['ImmiImages']['ImmiImage'][0] : undefined;

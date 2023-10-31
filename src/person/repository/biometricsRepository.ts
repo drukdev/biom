4bb48260ea1c1afb920b8c848d299982334cc401
Yogesh Pawar <yogesh.pawar@ayanworks.com>
2023-10-31 19:03:56 +0530

    fix: Increased maxBodyLength in regula image comparison API.

--- src/person/repository/biometricsRepository.ts
+++ src/person/repository/biometricsRepository.ts
@@ -12,19 +12,28 @@ export class BiometricRepository {
     private configService: ConfigService,
     private readonly als: AsyncLocalStorage<LoggerClsStore>,
     private readonly ndiLogger: NDILogger
-    ) {}
+  ) {}
   async compareImage(face1: Buffer, face2: ArrayBufferLike): Promise<number> {
     const ndiLogger = this.ndiLogger.getLoggerInstance(this.als);
     const apiBasePath = this.configService.get('BM_SDK_BASE_PATH');
     const sdk = new FaceSdk({ basePath: apiBasePath });
-    const matchingResponse = await sdk.matchingApi.match({
-      images: [
-        { type: ImageSource.LIVE, data: face1 },
-        { type: ImageSource.DOCUMENT_RFID, data: face1 },
-        { data: face2 }
-      ],
-      thumbnails: true
-    });
+    const maxBodyLength = 50 * 1024 * 1024; // 50MB in bytes
+
+    const matchingResponse = await sdk.matchingApi.match(
+      {
+        images: [
+          { type: ImageSource.LIVE, data: face1 },
+          { type: ImageSource.DOCUMENT_RFID, data: face1 },
+          { data: face2 }
+        ],
+        thumbnails: true
+      },
+      '',
+      {
+        maxBodyLength
+      }
+    );
+
     ndiLogger.log('-----------------------------------------------------------------');
     ndiLogger.log('                         Compare Results                         ');
     ndiLogger.log('-----------------------------------------------------------------');

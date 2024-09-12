16f2fccbede3de16e1a02dd059a7d66a23a912ad
sagarkhole4 <sagar.khole@ayanworks.com>
2024-09-12 17:50:24 +0530

    Updated module path

--- src/auth-token/DCRCAuthModule.ts
+++ src/auth-token/DCRCAuthModule.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import AuthTokenService from './auth-token';
 import { CacheModule } from '../cache/cache.module';
-import { AlsModule } from 'src/AsyncLocalStorage/als.module';
-import { LoggerModule } from 'src/logger/logger.module';
+import { AlsModule } from '../AsyncLocalStorage/als.module';
+import { LoggerModule } from '../logger/logger.module';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
